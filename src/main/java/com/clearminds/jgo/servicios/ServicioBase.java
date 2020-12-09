194ce7d722338d52e6c3eb447e9612e77321813d
jusegour <jusegour2015@gmail.com>
2020-12-09 10:34:33 -0500

    paquete de servicios, creados.

---
+++ src/main/java/com/clearminds/jgo/servicios/ServicioBase.java
@@ -0,0 +1,33 @@
+package com.clearminds.jgo.servicios;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import com.clearminds.jgo.bdd.ConexionBDD;
+import com.clearminds.jgo.excepciones.BDDException;
+
+public class ServicioBase {
+
+	private Connection conexion;
+
+	public void abrirConexion() throws BDDException {
+		try {
+			this.conexion = ConexionBDD.obtenerConexion();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void cerrarConexion() {
+		if (this.conexion != null) {
+			try {
+				this.conexion.close();
+				System.out.println("Conexion Cerrada");
+			} catch (SQLException e) {
+				e.printStackTrace();
+				System.err.println("Error al cerrar la conexion");
+			}
+		}
+	}
+
+}
---
+++ src/main/java/com/clearminds/jgo/servicios/ServicioEstudiante.java
@@ -0,0 +1,6 @@
+package com.clearminds.jgo.servicios;
+
+public class ServicioEstudiante extends ServicioBase {
+
+	
+}
