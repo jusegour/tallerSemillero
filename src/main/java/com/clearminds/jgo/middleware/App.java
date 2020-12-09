589af0badb8376ac042700080117cc2e78005851
jusegour <jusegour2015@gmail.com>
2020-12-09 09:44:25 -0500

    Version Inicial

---
+++ src/main/java/com/clearminds/jgo/middleware/App.java
@@ -0,0 +1,13 @@
+package com.clearminds.jgo.middleware;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
